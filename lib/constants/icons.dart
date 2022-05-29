c0981855ddb3fb5ae6b0c4297aee308b0c4c5ef4
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-05-29 12:34:33 +0000

    Added my app icons

---
+++ lib/constants/icons.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:fluttericon/elusive_icons.dart';
+import 'package:fluttericon/font_awesome5_icons.dart';
+import 'package:fluttericon/linearicons_free_icons.dart';
+
+class AppIcons {
+  static IconData privacy = Icons.security;
+  static IconData community = Elusive.group;
+  static IconData suitCase = LineariconsFree.briefcase;
+  static IconData doc = Icons.file_copy_outlined;
+  static IconData about = FontAwesome5.opera;
+  static IconData session = FontAwesome5.connectdevelop;
+  static IconData report = Icons.error;
+  static IconData envelope = FontAwesome5.envelope;
+  static IconData logout = Icons.exit_to_app;
+}
