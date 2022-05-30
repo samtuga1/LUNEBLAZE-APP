0fe83a174ad1bb99ffae66e5dc2a900081f258b8
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-05-30 14:20:20 +0000

    Updates(Added navigation methods)

---
+++ lib/core/view_models/settings_viewmodel.dart
@@ -0,0 +1,39 @@
+import 'package:stacked/stacked.dart';
+
+class SettingsViewModel extends BaseViewModel {
+  void navigateToPrivacy() {
+    // Moves to privacy screen
+  }
+
+  void navigateToCommunity() {
+    // Moves to community screen
+  }
+
+  void navigateToManagePage() {
+    // Moves to manaage screen
+  }
+  void navigateToInternship() {
+    // Moves to internship screen
+  }
+  void navigateToAbout() {
+    // Moves to about screen
+  }
+  void navigateToPrivacyPolicy() {
+    // Moves to privacy policy screen
+  }
+  void navigateToSessionPolicy() {
+    // Moves to session policy screen
+  }
+  void navigateToTermsOfUse() {
+    // Moves to terms of use screen
+  }
+  void navigateToReport() {
+    // Moves to report screen
+  }
+  void contactUs() {
+    // Moves to contact us screen
+  }
+  void logout() {
+    // Logs user out
+  }
+}
