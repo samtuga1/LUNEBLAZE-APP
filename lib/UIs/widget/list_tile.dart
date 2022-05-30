fb91c8243212864caf12c86f01bc822b20855eac
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-05-30 14:21:29 +0000

    refactored get list tile

---
+++ lib/UIs/widget/list_tile.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class GetListTile extends StatelessWidget {
+  const GetListTile({
+    Key? key,
+    this.icon,
+    this.title,
+    this.subtitle,
+    required this.onTap,
+  }) : super(key: key);
+
+  final IconData? icon;
+  final String? title;
+  final String? subtitle;
+  final Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap:
+          onTap, //This triggers the onTap function that is being passed as a parameter
+      child: ListTile(
+          leading: Icon(icon),
+          title: Text(title!),
+          subtitle: Text(subtitle!),
+          trailing: Icon(Icons.arrow_forward_ios)),
+    );
+  }
+}
