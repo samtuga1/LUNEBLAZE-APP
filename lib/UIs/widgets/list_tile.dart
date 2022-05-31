72c12907a284a171a23948fe60a8c5a66b6f2919
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-05-31 00:31:34 +0000

    Made some minor updates

--- lib/UIs/widgets/list_tile.dart
+++ lib/UIs/widgets/list_tile.dart
@@ -4,13 +4,13 @@ class GetListTile extends StatelessWidget {
   const GetListTile({
     Key? key,
     this.icon,
-    this.title,
+    required this.title,
     this.subtitle,
     required this.onTap,
   }) : super(key: key);
 
   final IconData? icon;
-  final String? title;
+  final String title;
   final String? subtitle;
   final Function() onTap;
 
@@ -21,9 +21,15 @@ class GetListTile extends StatelessWidget {
           onTap, //This triggers the onTap function that is being passed as a parameter
       child: ListTile(
           leading: Icon(icon),
-          title: Text(title!),
-          subtitle: Text(subtitle!),
-          trailing: Icon(Icons.arrow_forward_ios)),
+          title: Text(title),
+          subtitle: Text(
+            subtitle ?? '',
+            style: TextStyle(color: Colors.black54),
+          ),
+          trailing: Icon(
+            Icons.arrow_forward_ios,
+            color: Colors.black,
+          )),
     );
   }
 }
