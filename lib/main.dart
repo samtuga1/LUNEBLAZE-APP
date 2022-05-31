406083d5abcc6a2c5bb2b4f27000b337e8cca50f
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-05-31 00:28:56 +0000

    removed home route an added app route

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:luneblaze_app/UIs/views/setting_view.dart';
 
+import 'app/router.gr.dart';
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final _appRouter = AppRouter();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Luneblaze',
-      home: SettingsView(),
+      routerDelegate: _appRouter.delegate(),
+      routeInformationParser: _appRouter.defaultRouteParser(),
     );
   }
 }
