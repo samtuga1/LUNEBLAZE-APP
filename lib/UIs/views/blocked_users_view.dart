71d609ea8756fc3ceff26ac42f533a1afe3c7ac6
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-06-04 16:35:10 +0000

    update

---
+++ lib/UIs/views/blocked_users_view.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:luneblaze_app/UIs/widgets/global_appbar.dart';
+import 'package:stacked/stacked.dart';
+import '../../core/services/view_models/blocked_users_viewmodel.dart';
+
+class BlockedUsersView extends StatelessWidget {
+  const BlockedUsersView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<BlockedUsersViewModel>.reactive(
+      viewModelBuilder: () => BlockedUsersViewModel(),
+      builder: (context, model, child) => Scaffold(
+          appBar: GlobalAppBar(
+            title: 'BLOCKED USERS',
+            onTap: model.goBack,
+          ),
+          body: SafeArea(
+            child: Column(children: [
+              model.blockedUser!.isEmpty
+                  ? Center(
+                      child: Container(
+                        margin: const EdgeInsets.only(top: 40),
+                        child: Text(
+                          'There are no blocked users.',
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ),
+                    )
+                  : ListTile()
+            ]),
+          )),
+    );
+  }
+}
