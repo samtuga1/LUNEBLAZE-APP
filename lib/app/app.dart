f50062d7881d9d35fc4e2c09cbefa6b6d38a676b
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-06-04 16:34:09 +0000

    Added blockedUsersScreen to routes

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,3 +1,4 @@
+import 'package:luneblaze_app/UIs/views/blocked_users_view.dart';
 import 'package:luneblaze_app/UIs/views/privacy_settings_view.dart';
 import 'package:luneblaze_app/UIs/views/setting_view.dart';
 import 'package:stacked_services/stacked_services.dart';
@@ -8,6 +9,7 @@ import '../UIs/views/setting_view.dart';
   routes: <MaterialRoute>[
     MaterialRoute(initial: true, page: SettingsView),
     MaterialRoute(page: PrivacySettingsView),
+    MaterialRoute(page: BlockedUsersView),
   ],
   dependencies: [
     LazySingleton(classType: NavigationService),
