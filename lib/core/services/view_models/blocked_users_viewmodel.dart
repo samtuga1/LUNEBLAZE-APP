8dc0e37f2bbe9fba33817a6c754e074cdff6e38b
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-06-05 13:26:58 +0000

    added comment

--- lib/core/services/view_models/blocked_users_viewmodel.dart
+++ lib/core/services/view_models/blocked_users_viewmodel.dart
@@ -3,7 +3,8 @@ import 'package:stacked_services/stacked_services.dart';
 
 class BlockedUsersViewModel extends BaseViewModel {
   final _navigate = NavigationService();
-  List? blockedUser;
+  //Initially empty blocked users
+  List? blockedUser = [];
   void goBack() {
     _navigate.popRepeated(1);
   }
