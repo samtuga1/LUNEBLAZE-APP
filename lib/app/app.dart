9123f4d5742e466814adf47b6fed5198488990e6
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-06-06 09:22:14 +0000

    added another route

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,4 +1,5 @@
 import 'package:luneblaze_app/UIs/views/blocked_users_view.dart';
+import 'package:luneblaze_app/UIs/views/get_notified_view.dart';
 import 'package:luneblaze_app/UIs/views/privacy_settings_view.dart';
 import 'package:luneblaze_app/UIs/views/setting_view.dart';
 import 'package:stacked_services/stacked_services.dart';
@@ -10,6 +11,7 @@ import '../UIs/views/setting_view.dart';
     MaterialRoute(initial: true, page: SettingsView),
     MaterialRoute(page: PrivacySettingsView),
     MaterialRoute(page: BlockedUsersView),
+    MaterialRoute(page: GetNotifiedView),
   ],
   dependencies: [
     LazySingleton(classType: NavigationService),
