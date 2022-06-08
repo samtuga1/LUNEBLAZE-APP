6f05dfc38a373a8339f87c960669bdff7594836e
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-06-08 15:46:00 +0000

    Refactored my code

--- lib/core/services/view_models/get_notified_viewmodel.dart
+++ lib/core/services/view_models/get_notified_viewmodel.dart
@@ -4,10 +4,13 @@ import 'package:stacked_services/stacked_services.dart';
 
 class GetNotifiedViewModel extends BaseViewModel {
   final _navigation = locator<NavigationService>();
+  // ignore: non_constant_identifier_names
   List<String> course_choice_options = ['Tech', 'BBA', 'BCA', 'Bcom'];
+  // ignore: non_constant_identifier_names
+  String? selected_course;
 
   void selectCourseOption(String value) {
-    course_choice_options[0] = value;
+    selected_course = value;
     notifyListeners();
   }
 
@@ -17,30 +20,34 @@ class GetNotifiedViewModel extends BaseViewModel {
     'Physics',
     'Electrical'
   ];
+  String? selected_study_option;
 
   void selectStudyOption(String value) {
-    study_choice_options[0] = value;
+    selected_study_option = value;
     notifyListeners();
   }
 
   List<String> year_passing_options = ['2022', '2021', '2020', '2019'];
+  String? selected_passing_year;
 
   void selectYearOfPass(String value) {
-    year_passing_options[0] = value;
+    selected_passing_year = value;
     notifyListeners();
   }
 
   List<String> percentage_choice_options = ['>40', '>50', '>60', '>70'];
+  String? selected_percentage_choice;
 
   void selectpercentageOption(String value) {
-    percentage_choice_options[0] = value;
+    selected_percentage_choice = value;
     notifyListeners();
   }
 
   List<String> starting_year_options = ['2022', '2021', '2020', '2019'];
+  String? selected_starting_year_option;
 
   void selectStartingYearOption(String value) {
-    starting_year_options[0] = value;
+    selected_starting_year_option = value;
     notifyListeners();
   }
 
@@ -48,11 +55,13 @@ class GetNotifiedViewModel extends BaseViewModel {
 
   bool internshipSwitch = false;
   bool jobSwitch = false;
+  // Switching for internship
   void switchInternshipMode(bool switchValue) {
     internshipSwitch = switchValue;
     notifyListeners();
   }
 
+// Switching for job mode
   void switchJobMode(bool switchValue) {
     jobSwitch = switchValue;
     notifyListeners();
