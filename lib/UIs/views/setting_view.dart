9d38e3ba12a07a9446f874fd5ca2870067e7379e
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-06-09 13:55:19 +0000

    update

--- lib/UIs/views/setting_view.dart
+++ lib/UIs/views/setting_view.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:luneblaze_app/UIs/widgets/global_appbar.dart';
 import 'package:stacked/stacked.dart';
 import 'package:luneblaze_app/constants/icons.dart';
-import '../../core/services/view_models/settings_viewmodel.dart';
+import '../../core/view_models/settings_viewmodel.dart';
 import '../widgets/header_title.dart';
 import '../widgets/list_tile.dart';
 
