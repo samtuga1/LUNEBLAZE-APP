00b04a5b0b592842984a66849fa4ae1eb23849ce
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-06-09 13:54:05 +0000

    update

---
+++ lib/core/services/api_services.chopper.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'api_services.dart';
+
+// **************************************************************************
+// ChopperGenerator
+// **************************************************************************
+
+// ignore_for_file: always_put_control_body_on_new_line, always_specify_types, prefer_const_declarations, unnecessary_brace_in_string_interps
+class _$api_services extends api_services {
+  _$api_services([ChopperClient? client]) {
+    if (client == null) return;
+    this.client = client;
+  }
+
+  @override
+  final definitionType = api_services;
+
+  @override
+  Future<Response<dynamic>> postPrivacyData(Map<String, String> body) {
+    final $url = '/app/profile.json';
+    final $body = body;
+    final $request = Request('POST', $url, client.baseUrl, body: $body);
+    return client.send<dynamic, dynamic>($request);
+  }
+}
