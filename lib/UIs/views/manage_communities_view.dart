9767459c0f1cd9f03f89fc338da1134fa7b5e79e
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-06-12 11:14:56 +0000

    update

---
+++ lib/UIs/views/manage_communities_view.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:luneblaze_app/UIs/widgets/global_appbar.dart';
+import 'package:luneblaze_app/UIs/widgets/list_tile.dart';
+import 'package:stacked/stacked.dart';
+
+import '../../core/view_models/manage_comunities_viewmodel.dart';
+
+class ManageCommunitiesView extends StatelessWidget {
+  const ManageCommunitiesView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<ManageCommunitiesViewModel>.reactive(
+      onModelReady: (model) => model.fakeDelay(),
+      viewModelBuilder: () => ManageCommunitiesViewModel(),
+      builder: (context, model, child) => Scaffold(
+        backgroundColor: Color(0xFFEDEDED),
+        appBar: GlobalAppBar(
+          title: 'MANAGE COMMUNITIES',
+          onTap: model.goBack,
+        ),
+        body: Column(
+          children: [
+            Container(
+              color: Colors.white,
+              margin: const EdgeInsets.only(top: 6),
+              child: GetListTile(
+                icon: Icons.computer,
+                title: 'Create Community',
+                onTap: model.createCommunity,
+              ),
+            ),
+            model.loading
+                ? Expanded(
+                    child: Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  )
+                : Text(''),
+          ],
+        ),
+      ),
+    );
+  }
+}
