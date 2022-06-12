7fee935d291aa6a38815ed8934ca8921fbfc3c70
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-06-12 11:14:37 +0000

    update

---
+++ lib/core/view_models/manage_comunities_viewmodel.dart
@@ -0,0 +1,25 @@
+import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+class ManageCommunitiesViewModel extends BaseViewModel {
+  final navigation = NavigationService();
+  void goBack() {
+    navigation.popRepeated(1);
+  }
+
+  bool loading = false;
+
+// Fake loading indication function
+  Future<void> fakeDelay() async {
+    loading = true;
+    Future.delayed(Duration(milliseconds: 900), () {
+      loading = false;
+      notifyListeners();
+    });
+  }
+
+  // Navigate to create community
+  void createCommunity() {
+    //
+  }
+}
