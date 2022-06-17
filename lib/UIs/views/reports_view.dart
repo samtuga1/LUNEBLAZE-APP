aa99f83da6c91c35f6dc4234d858b4398357f792
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-06-17 21:05:37 +0000

    update

--- lib/UIs/views/reports_view.dart
+++ lib/UIs/views/reports_view.dart
@@ -170,7 +170,10 @@ class ReportsView extends StatelessWidget {
                     Align(
                       alignment: Alignment.bottomCenter,
                       child: GestureDetector(
-                        onTap: model.validateAndReport,
+                        onTap: () {
+                          FocusScope.of(context).unfocus();
+                          model.validateAndReport();
+                        },
                         child: Container(
                           color: Color(0xFFC7C7C7).withOpacity(0.15),
                           padding: const EdgeInsets.symmetric(
