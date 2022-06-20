f66bbc9c9b4a8e456f0cf45acfb240e72fd61af8
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-06-20 13:19:47 +0000

    update

--- lib/core/view_models/create_institutions_viewmodel.dart
+++ lib/core/view_models/create_institutions_viewmodel.dart
@@ -1,13 +1,51 @@
+import 'dart:developer';
+
 import 'package:flutter/cupertino.dart';
 import 'package:luneblaze_app/app/app.locator.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
+import 'package:luneblaze_app/core/services/api_services.dart';
+
+import '../../app/app.router.dart';
 
 class CreateInstitutionsViewModel extends BaseViewModel {
-  bool switchCollege = false;
   final formKey = GlobalKey<FormState>();
   final navigation = locator<NavigationService>();
   int institutionNameLength = 0;
+  bool loading = false;
+  bool switchCollege = false;
+  String institutionName = '';
+  String businessEmail = '';
+  String businessPhone = '';
+  String website = '';
+
+  Future<void> validateAndRegister() async {
+    if (!formKey.currentState!.validate()) {
+      return;
+    } else {
+      loading = true;
+      notifyListeners();
+      try {
+        final response = await api_services.create().postInstitutionData({
+          'user_id': '1',
+          'venue_name': institutionName,
+          'venue_email': businessEmail,
+          'venue_contact': businessPhone,
+          'website_link': website,
+          'is_college': switchCollege ? '1' : '0',
+        });
+        loading = false;
+        navigation.navigateTo(Routes.otpView);
+        notifyListeners();
+      } catch (error) {
+        loading = false;
+        notifyListeners();
+        log(error.toString());
+        // Handling of exceptions
+      }
+    }
+  }
+
   void toggleInstitutionNameLength(int valueLength) {
     institutionNameLength = valueLength;
     notifyListeners();
@@ -15,10 +53,6 @@ class CreateInstitutionsViewModel extends BaseViewModel {
 
   void goBack() {
     navigation.popRepeated(1);
-  }
-
-  void validateAndRegister() {
-    formKey.currentState!.validate();
   }
 
   void switchCollegePressed(value) {
